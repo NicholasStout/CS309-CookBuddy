bd18212aa7bc2d9dba62e31d8c72a77036940899
Nicholas Stout <nsstout@iastate.edu>
2018-09-24 08:15:27 -0500

    Begin creation of the User api callable.

---
+++ Backend/demo2_backend/src/main/java/com/demo2/backend/AppController.java
@@ -0,0 +1,20 @@
+package com.demo2.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.demo2.backend.User;
+import com.demo2.backend.UserRepository;
+
+@Controller
+@RequestMapping(path="/table")
+public class AppController {
+	@Autowired
+	private UserRepository uRepo;
+	
+	
+}
---
+++ Backend/demo2_backend/src/main/java/com/demo2/backend/User.java
@@ -0,0 +1,32 @@
+package com.demo2.backend;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+@table()
+public class User {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+	
+	private String name;
+	
+	public void setName (String n) {
+		name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Integer getID() {
+		return id;
+	}
+}
