764a102af09f1d64ac5972774d9e003c840b0dd9
jackc1 <jackc1@iastate.edu>
2018-10-02 16:52:41 -0500

    fixing some merge issues

---
+++ Experiments/jcreighton/Demo2/app/src/main/java/com/example/jackc/demo2_frontend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jackc.demo2_frontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
