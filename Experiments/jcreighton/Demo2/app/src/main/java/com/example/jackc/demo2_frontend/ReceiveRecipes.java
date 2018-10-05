95516d6ea73cbf2bce97e5878a08b116e3de7c25
jackc1 <jackc1@iastate.edu>
2018-10-04 19:06:47 -0500

    Server response handlers

--- Experiments/jcreighton/Demo2/app/src/main/java/com/example/jackc/demo2_frontend/ReceiveRecipes.java
+++ Experiments/jcreighton/Demo2/app/src/main/java/com/example/jackc/demo2_frontend/ReceiveRecipes.java
@@ -3,11 +3,24 @@ package com.example.jackc.demo2_frontend;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.example.jackc.demo2_frontend.app.AppController;
+import com.example.jackc.demo2_frontend.net_utils.Const;
 
 public class ReceiveRecipes extends AppCompatActivity {
+
+    private String TAG = ReceiveRecipes.class.getSimpleName();
     private Button btnGetRecipe, btnAddRecipe;
+    private TextView receivedIngredients, receivedTags;
 
 
     @Override
@@ -15,9 +28,10 @@ public class ReceiveRecipes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_recipes);
 
-        btnGetRecipe = findViewById(R.id.buttonGetRecipe);
-        btnAddRecipe = findViewById(R.id.buttonAddNewRecipe);
-
+        btnGetRecipe = (Button) findViewById(R.id.buttonGetRecipe);
+        btnAddRecipe = (Button) findViewById(R.id.buttonAddNewRecipe);
+        receivedIngredients = (TextView) findViewById(R.id.receivedIngredients);
+        receivedTags = (TextView) findViewById(R.id.receivedTags);
 
         btnAddRecipe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +40,32 @@ public class ReceiveRecipes extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        btnGetRecipe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                makeStringRequest();
+            }
+        });
+
+
+    }
+
+    private void makeStringRequest(){
+        StringRequest strReq = new StringRequest(Request.Method.GET, Const.URL_STRING_REQ, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response.toString());
+                receivedIngredients.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(strReq);
     }
 }
 
