4041ef651c36f91a2aee35133e7615f76934937b
morellas <morellas@iastate.edu>
2018-10-31 15:27:30 -0500

    Created a new HomeActivity that has a sliding menu drawer

--- Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/RegisterUser.java
+++ Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/RegisterUser.java
@@ -1,44 +1,30 @@
-package com.example.primaryfolder.cookbuddy;
+package com.example.primaryfolder.cookbuddy.activities;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.primaryfolder.cookbuddy.R;
 import com.example.primaryfolder.cookbuddy.app.AppController;
-import com.example.primaryfolder.cookbuddy.net_utils.Const;
 import com.android.volley.Request;
-import android.view.View.OnClickListener;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
-import static com.android.volley.Request.Method.POST;
 
 public class RegisterUser extends AppCompatActivity {
 
@@ -180,7 +166,7 @@ public class RegisterUser extends AppCompatActivity {
                     AppController.getInstance().addToRequestQueue(jsonObjReq);
 
                     // Redirect to Home Page
-                    Intent j = new Intent(RegisterUser.this, Home.class);
+                    Intent j = new Intent(RegisterUser.this, HomeActivity.class);
                     startActivity(j);
                 }
             }
