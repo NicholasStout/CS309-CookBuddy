0c33f5f6108debf5259d1b9bd88814cbac960e25
chris <crice@iastate.edu>
2018-11-01 20:00:25 -0500

    created a premessaging image, for user to enter a username before joining chat. Untested as of now

--- Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/MessagingActivity.java
+++ Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/MessagingActivity.java
@@ -2,14 +2,35 @@ package com.example.primaryfolder.cookbuddy.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.primaryfolder.cookbuddy.R;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class MessagingActivity extends AppCompatActivity {
+    Button b1, b2;
+    EditText e1, e2;
+    TextView t1;
+
+    private WebSocketClient cc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+
+
+
     }
 }
