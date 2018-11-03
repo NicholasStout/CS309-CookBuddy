0a7cf7f0123c5fc7db8be5fecf3c4b5fc223867d
Nicholas Stout <nsstout@iastate.edu>
2018-11-02 22:02:52 -0500

    Fix JSON response

--- Backend/demo2_backend/src/main/java/com/demo2/backend/Response.java
+++ Backend/demo2_backend/src/main/java/com/demo2/backend/Response.java
@@ -15,7 +15,7 @@ public class Response {
 		Map<String, String> ret = new HashMap<String, String>();
 		ret.put("Error", "0");
 		ret.put("Response", "Success!");
-		ret.put("user_id", new Integer(i).toString());
+		ret.put("userId", new Integer(i).toString());
 		return ret;
 	}
 	
@@ -23,15 +23,15 @@ public class Response {
 		Map<String, String> ret = new HashMap<String, String>();
 		ret.put("Error", "0");
 		ret.put("name", u.getName());
-		ret.put("id", new Integer(u.getID()).toString());
+		ret.put("userId", new Integer(u.getID()).toString());
 		return ret;
 	}
 	
 	public static Map<String, String> recipe (Recipe r) {
 		Map<String, String> ret = new HashMap<String, String>();
 		ret.put("Error", "0");
-		ret.put("recipe_id",  new Integer(r.getId()).toString());
-		ret.put("recipe_name", r.getRecipeName());
+		ret.put("recipeId",  new Integer(r.getId()).toString());
+		ret.put("recipeName", r.getRecipeName());
 		ret.put("instructions", r.getInstructions());
 		ret.putAll(user(r.getUser()));
 		return ret;
