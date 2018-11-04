d94f194bdb8df46f6541536eb07289dbe6af7f36
morellas <morellas@iastate.edu>
2018-11-04 16:28:39 -0600

    Added functionality for the floating acaction button

--- Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/MainActivity.java
+++ Frontend/app/src/main/java/com/example/primaryfolder/cookbuddy/activities/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.primaryfolder.cookbuddy.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -45,8 +45,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this, AddRecipe.class);
+                startActivity(i);
             }
         });
 
